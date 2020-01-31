6ca682a35d65e4aeb767a9b10f8c0826b08bcc6c
Kevin Jensen <jensenk28@yahoo.com>
2020-01-31 13:50:56 -0800

    finished mvp, i think

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import styled from "styled-components";
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  `
+
+
+
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
-      <header>
+      <Header>
         <h1>Welcome to the ultimate fan site!</h1>
         <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          alt="rick"
+          alt="Rick"
         />
-      </header>
+      </Header>
       <Link to='/characters'>Characters</Link>
     </section>
   );
